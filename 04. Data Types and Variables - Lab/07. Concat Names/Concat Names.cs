31a3cca4216bc4a55063c74a1373bbd36de1e2a7
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-05-28 00:49:30 +0300

    Create Concat Names.cs

---
+++ 04. Data Types and Variables - Lab/07. Concat Names/Concat Names.cs
@@ -0,0 +1,7 @@
+// Read two names and a delimiter. Print the names joined by the delimiter.
+
+string firstName = Console.ReadLine();
+string secondName = Console.ReadLine();
+string delimiter = Console.ReadLine();
+
+Console.WriteLine(firstName + delimiter + secondName);
