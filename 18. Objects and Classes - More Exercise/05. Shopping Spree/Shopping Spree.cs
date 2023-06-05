f150fcdfa8b92f79e33c316941ed199d037db738
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-06-06 00:40:10 +0300

    Update Shopping Spree.cs

--- 18. Objects and Classes - More Exercise/05. Shopping Spree/Shopping Spree.cs
+++ 18. Objects and Classes - More Exercise/05. Shopping Spree/Shopping Spree.cs
@@ -7,7 +7,6 @@
 // of appearance and all products that they have bought, also in order of appearance. If nothing was bought, print the
 // name of the person followed by "Nothing bought".
 
-
 namespace _05._Shopping_Spree
 {
     using System;
@@ -21,17 +20,51 @@ namespace _05._Shopping_Spree
             FillingPerson(persons);
             FillingProduct(products);
 
+            BayProducts(persons , products);
+            PrintWhatTheyBay(persons);
+
+        }
+
+        public static void PrintWhatTheyBay(List<Person> persons)
+        {
+            foreach (Person person in persons)
+            {
+                if (person.bagOfProducts.Count == 0)
+                {
+                    Console.WriteLine($"{person.Name} - Nothing bought");
+                }
+                else
+                {
+                    Console.WriteLine(person);
+
+                }
+            }
+        }
+        public static void BayProducts(List<Person>persons , List<Product> products)
+        {
             string curentArg;
 
             while ((curentArg = Console.ReadLine()) != "END")
             {
                 string[] curentCouple = curentArg
-                    .Split(" " , StringSplitOptions.RemoveEmptyEntries)
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
                     .ToArray();
                 string name = curentCouple[0];
                 string item = curentCouple[1];
-            }
+                int price = products.First(x => x.Name == item).Cost;
 
+                if (persons.Any(x => x.Name == name) && persons.First(x => x.Name == name).Money >= price)
+                {
+                    persons.First(x => x.Name == name).AddItem(item);
+                    persons.First(x => x.Name == name).Money -= price;
+                    Console.WriteLine($"{name} bought {item}");
+                }
+                else
+                {
+                    string curentName = persons.First(x => x.Name == name).Name;
+                    Console.WriteLine($"{name} can't afford {item}");
+                }
+            }
         }
 
         public static void FillingProduct(List<Product> products)
@@ -39,9 +72,10 @@ namespace _05._Shopping_Spree
             string[] productInt = Console.ReadLine()
                 .Split(";", StringSplitOptions.RemoveEmptyEntries)
                 .ToArray();
+
             for (int i = 0; i < productInt.Length; i++)
             {
-                string[] curentProduct = Console.ReadLine()
+                string[] curentProduct = productInt[i]
                     .Split("=", StringSplitOptions.RemoveEmptyEntries)
                     .ToArray();
                 Product product = new Product(curentProduct[0], int.Parse(curentProduct[1]));
@@ -53,12 +87,16 @@ namespace _05._Shopping_Spree
             string[] peopleInt = Console.ReadLine()
               .Split(";", StringSplitOptions.RemoveEmptyEntries)
               .ToArray();
+
             for (int i = 0; i < peopleInt.Length; i++)
             {
-                string[] curentPerson = Console.ReadLine()
+                string[] curentPerson = peopleInt[i]
                     .Split("=", StringSplitOptions.RemoveEmptyEntries)
                     .ToArray();
-                Person person = new Person(curentPerson[0], int.Parse(curentPerson[1]));
+                string name = curentPerson[0];
+                int money = int.Parse(curentPerson[1]);
+
+                Person person = new Person(name, money);
                 persons.Add(person);
             }
         }
@@ -86,11 +124,22 @@ namespace _05._Shopping_Spree
             Name = name;
             Money = money;
 
-            bagOfProducts = new List<string>();
+            this.bagOfProducts = new List<string>();
         }
 
         public string Name { get; set; }
 
         public int Money { get; set; }
+
+        public void AddItem(string item)
+        {
+
+            this.bagOfProducts.Add(item);
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} - " + string.Join(", ", this.bagOfProducts);
+        }
     }
 }
