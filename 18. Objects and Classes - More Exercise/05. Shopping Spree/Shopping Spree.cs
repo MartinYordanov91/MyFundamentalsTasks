a76797f7be5c5678bcf2e6667a2ccb2e9f4b3dec
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-06-06 00:47:53 +0300

    Update Shopping Spree.cs

--- 18. Objects and Classes - More Exercise/05. Shopping Spree/Shopping Spree.cs
+++ 18. Objects and Classes - More Exercise/05. Shopping Spree/Shopping Spree.cs
@@ -1,11 +1,3 @@
-// Create two classes: class Person and class Product. Each person should have a name, money and a bag of
-// products. Each product should have a name and a cost.
-// Create a program, in which each command corresponds to a person buying a product. If the person can afford a
-// product, add it to his bag. If a person doesn't have enough money, print an appropriate message: "{Person} can't
-// afford {Product}".
-// On the first two lines, you are given all people and all products. After all purchases, print every person in the order
-// of appearance and all products that they have bought, also in order of appearance. If nothing was bought, print the
-// name of the person followed by "Nothing bought".
 
 namespace _05._Shopping_Spree
 {
