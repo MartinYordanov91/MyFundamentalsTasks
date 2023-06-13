b87e8b82d17c0cd79d09e512cba0b18a598118b0
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-06-13 23:13:59 +0300

    Create Digits, Letters and Other.cs

---
+++ 22. Text Processing - Lab/05. Digits, Letters and Other/Digits, Letters and Other.cs
@@ -0,0 +1,21 @@
+namespace _05._Digits__Letters_and_Other
+{
+    using System;
+    using System.Text;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] input = Console.ReadLine().ToArray ();
+
+            var sb = new StringBuilder();
+
+            sb.AppendLine(string.Join("",input.Where(x => char.IsDigit(x))));
+            sb.AppendLine(string.Join("",input.Where(x => char.IsLetter(x))));
+            sb.AppendLine(string.Join("",input.Where(x => !char.IsLetter(x) && !char.IsDigit(x))));
+            
+            Console.WriteLine(sb);
+        }
+    }
+}
