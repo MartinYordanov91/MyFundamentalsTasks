466204ef7f17318be1e7491cdba728bcb8c0a4a2
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-06-17 21:59:14 +0300

    Create HTML.cs

---
+++ 24. Text Processing - More Exercise/05. HTML/HTML.cs
@@ -0,0 +1,30 @@
+namespace _08._Letters_Change_Numbers
+{
+    using System;
+    using System.Text;
+    using System.Text.RegularExpressions;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine("<h1>");
+            sb.AppendLine($"    {Console.ReadLine()}");
+            sb.AppendLine("</h1>");
+            sb.AppendLine("<article>");
+            sb.AppendLine($"    {Console.ReadLine()}");
+            sb.AppendLine("</article>");
+
+            string end = string.Empty;
+            while ((end = Console.ReadLine())!= "end of comments")
+            {
+                sb.AppendLine("<div>");
+                sb.AppendLine($"    {end}");
+                sb.AppendLine("</div>");
+            }
+
+            Console.WriteLine(sb);
+        }
+    }
+}
